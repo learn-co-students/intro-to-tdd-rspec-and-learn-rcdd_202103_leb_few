e6f7df36350b7ba562367e5ae35b4ab09ff6b222
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-17 08:37:37 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  def current_age_for_birth_year(birth_year)
+  Time.now.year - birth_year
+end
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
